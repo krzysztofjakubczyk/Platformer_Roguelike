4ab8559ba07c79f89a7b9cf4cc4aee4f717e6916
Daniel1nOw <87197476+Daniel1nOw@users.noreply.github.com>
2024-02-26 19:36:27 +0100

    cameraMovement260201

--- Assets/Scripts/FollowTarget.cs
+++ Assets/Scripts/FollowTarget.cs
@@ -1,22 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class FollowTarget : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(0, 0, -10f);
+    private Vector3 offset = new Vector3(2.5f, 0, -10f);
     private Vector3 velocity = Vector3.zero;
 
     [SerializeField] float smoothTime = 0.15f;
     [SerializeField] Transform target;
 
+    bool camSide;
+    bool notYetChanged_Y;
+
     private void Awake()
     {
         Application.targetFrameRate = 120;
     }
+
+    private void Start()
+    {
+        target.GetComponent<PlayerMovement>().directionChange += dirChange;
+
+        camSide = true;
+        notYetChanged_Y = true;
+    }
+
     private void Update()
     {
-        Vector3 targetPosition = target.position + offset;
+        if(target.GetComponent<PlayerMovement>().IsGrounded() && notYetChanged_Y)
+        {
+            offset.y = target.transform.position.y + 2f;
+            notYetChanged_Y=false;
+            Invoke(nameof(Ychange), 0.3f);
+
+        }
+        Vector3 targetPosition = new Vector3( target.position.x + offset.x,  offset.y, -10);
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+
+    void dirChange()
+    {
+        if (camSide == true)        camSide = false;
+        else if (camSide == false)  camSide = true;
+
+        if (camSide)
+            offset += new Vector3(5, 0, 0);
+        else
+            offset -= new Vector3(5, 0, 0);
+
+        //smoothTime = 0.5f;
+        //Invoke(nameof(timeBack), 1f);
+
+    }
+
+    void timeBack()
+    {
+        smoothTime = 0.15f;
+    }
+
+    void Ychange()
+    {
+        notYetChanged_Y = true;
     }
 }
