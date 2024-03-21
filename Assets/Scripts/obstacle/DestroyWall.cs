058ddf6a6ed745bc87ae4ca377eb5fbe672e8f46
krzysztofjakubczyk <28901@s.pm.szczecin.pl>
2024-03-21 10:20:49 +0100

    Obstacles

--- Assets/Scripts/obstacle/DestroyWall.cs
+++ Assets/Scripts/obstacle/DestroyWall.cs
@@ -7,9 +7,11 @@ public class DestroyWall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.DrawRay((transform.position + transform.right), transform.right, Color.red, 0.2f);
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            RaycastHit2D hit = Physics2D.Raycast((transform.position + transform.right * 1), transform.right, 0.1f);
+            RaycastHit2D hit = Physics2D.Raycast((transform.position + transform.right), transform.right, 0.2f);
+            
             if (hit.collider != null && hit.collider.tag == "ObstacleWall")
             {
                 // Zniszcz collider, który jest na drodze
