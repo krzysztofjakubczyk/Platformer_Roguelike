8bbcf9884e149cd66b048fbb4a5c3965b6b12d22
Daniel1nOw <87197476+Daniel1nOw@users.noreply.github.com>
2024-03-26 13:44:30 +0100

    qwert

--- Assets/MoveSet/MovementFin.cs
+++ Assets/MoveSet/MovementFin.cs
@@ -5,22 +5,30 @@ using UnityEngine;
 public class MovementFin : MonoBehaviour
 {
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask enemyLayer;
+    [SerializeField] private LayerMask pustwarstwa;
     [SerializeField] float speed = 4f;
     [SerializeField] float jumpingPower = 7f;
+    [SerializeField] float dashPower = 5f;
 
     Rigidbody2D rb;
     BoxCollider2D boxCollider;
     float horizontal;
+    float lastDirection;
 
     bool isJumping, willJump;
     bool isGrounded, wasGrounded;
     bool coyoteTime;
     [SerializeField]float coyoteTimeDuration = 1;
+    bool isDashing;
+    bool sideJump;
 
+    bool isJumpingRight, isJumpingLeft;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
+        Application.targetFrameRate = 120;
     }
 
     void Update()
@@ -34,7 +42,8 @@ public class MovementFin : MonoBehaviour
     private void FixedUpdate()
     {
         // moving player horizontally:
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        if(!isDashing && !sideJump)
+            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 
 
@@ -47,12 +56,19 @@ public class MovementFin : MonoBehaviour
     {
         bool ZPressed = false;
         horizontal = Input.GetAxisRaw("Horizontal");
+        if (horizontal != 0)
+            lastDirection = horizontal;
         isGrounded = IsGrounded();
         if (Input.GetKeyDown(KeyCode.Z))
             ZPressed = true;
 
-        // if player run out of platform (but didnt jump) activate coyoteTime
+        if (Input.GetKeyDown(KeyCode.X))
+        {
 
+            isDashing = true;
+        }
+
+        // if player run out of platform (but didnt jump) activate coyoteTime
         if (!isGrounded && wasGrounded && !Input.GetKey(KeyCode.Z))
         {
             coyoteTime = true;
@@ -81,11 +97,25 @@ public class MovementFin : MonoBehaviour
             willJump = false;
         }
 
-        if (ZPressed && rb.velocity.y > 0f)
+        if (Input.GetKeyUp(KeyCode.Z) && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
         #endregion
+
+        //wall jump
+        if (Physics2D.Raycast(transform.position, Vector2.right, .4f, groundLayer) && !isGrounded && ZPressed)
+        {
+            isJumpingLeft = true;
+            sideJump = true;
+        }
+
+
+        if (Physics2D.Raycast(transform.position, Vector2.left, .4f, groundLayer) && !isGrounded && ZPressed)
+        {
+            isJumpingRight = true;
+            sideJump = true;
+        }
 
         wasGrounded = isGrounded;
     }
@@ -97,6 +127,25 @@ public class MovementFin : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
             isJumping=false;
         }
+        else if (isJumpingLeft)
+        {
+            rb.velocity = new Vector2(-jumpingPower, jumpingPower);
+            isJumpingLeft = false;
+            Invoke(nameof(EndSideJump), 0.1f);
+        }
+        else if (isJumpingRight)
+        {
+            rb.velocity = new Vector2(jumpingPower, jumpingPower);
+            isJumpingRight = false;
+            Invoke(nameof(EndSideJump), 0.1f);
+        }
+
+        if (isDashing)
+        {
+            rb.AddForce(new Vector2(lastDirection * dashPower, 0),ForceMode2D.Impulse);
+            boxCollider.excludeLayers = enemyLayer;
+            Invoke(nameof(EndDash), 0.1f);
+        }
     }
 
     void EndCoyoteTime()
@@ -104,4 +153,14 @@ public class MovementFin : MonoBehaviour
         coyoteTime = false;
     }
 
+    void EndDash()
+    {
+        isDashing = false;
+        boxCollider.excludeLayers = pustwarstwa;
+    }
+
+    void EndSideJump()
+    {
+        sideJump = false;
+    }
 }
