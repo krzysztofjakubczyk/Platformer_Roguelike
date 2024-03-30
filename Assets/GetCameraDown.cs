c4b992e617550cf066ca75d1f2a9f7b85cb52b79
krzysztofjakubczyk <28901@s.pm.szczecin.pl>
2024-03-30 15:21:34 +0100

    lol

--- Assets/GetCameraDown.cs
+++ Assets/GetCameraDown.cs
@@ -7,17 +7,14 @@ public class GetCameraDown : MonoBehaviour
     [SerializeField] CinemachineVirtualCamera CameraDown;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && gameObject.CompareTag("StartCameraMoveDown"))
         {
             mainCamera.gameObject.SetActive(false);
             CameraDown.gameObject.SetActive(true);
-            CameraDown.GetCinemachineComponent<Cinemachine.CinemachineTransposer>().m_FollowOffset = mainCamera.GetCinemachineComponent<Cinemachine.CinemachineTransposer>().m_FollowOffset;
-            CameraDown.GetCinemachineComponent<Cinemachine.CinemachineTransposer>().m_FollowOffset.y = -2f;
+            //CameraDown.GetCinemachineComponent<Cinemachine.CinemachineTransposer>().m_FollowOffset = mainCamera.GetCinemachineComponent<Cinemachine.CinemachineTransposer>().m_FollowOffset;
+            //CameraDown.GetCinemachineComponent<Cinemachine.CinemachineTransposer>().m_FollowOffset.y = -2f;
         }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
+        else if(collision.CompareTag("Player") && gameObject.CompareTag("EndCameraMoveDown"))
         {
             mainCamera.gameObject.SetActive(true);
             CameraDown.gameObject.SetActive(false);// Mo¿esz dostosowaæ tê wartoœæ
