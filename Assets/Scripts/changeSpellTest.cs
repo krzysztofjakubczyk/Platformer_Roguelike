2445055054efa48b77cb834ce6a5e5b766049928
Daniel1nOw <87197476+Daniel1nOw@users.noreply.github.com>
2024-05-12 16:23:43 +0200

    12.05-p1

--- Assets/Scripts/changeSpellTest.cs
+++ Assets/Scripts/changeSpellTest.cs
@@ -5,33 +5,42 @@ using UnityEngine;
 public class changeSpellTest : MonoBehaviour
 {
     [SerializeField] GameObject player;
-    [SerializeField] GameObject spell;
+    [SerializeField] GameObject spell1;
+    [SerializeField] GameObject spell2;
+    [SerializeField] GameObject spell3;
+    [SerializeField] GameObject spell4;
+    [SerializeField] GameObject spell5;
 
-    bool canTakeSpell;
 
 
     // Update is called once per frame
     void Update()
     {
-        if (canTakeSpell)
+        if (Input.GetKeyUp(KeyCode.Alpha1) && spell1 != null)
         {
-            if (Input.GetKeyUp(KeyCode.Q))
-            {
-                player.GetComponent<SpellManager>().setSpell(spell);
-                print(spell.name + "  SET");
-            }
+            player.GetComponent<SpellManager>().setSpell(spell1);
+            print(spell1.name + "  SET");
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha2) && spell2 != null)
+        {
+            player.GetComponent<SpellManager>().setSpell(spell2);
+            print(spell2.name + "  SET");
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha3) && spell1 != null)
+        {
+            player.GetComponent<SpellManager>().setSpell(spell3);
+            print(spell3.name + "  SET");
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha4) && spell1 != null)
+        {
+            player.GetComponent<SpellManager>().setSpell(spell4);
+            print(spell4.name + "  SET");
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha5) && spell1 != null)
+        {
+            player.GetComponent<SpellManager>().setSpell(spell5);
+            print(spell5.name + "  SET");
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.transform.tag == "Player")
-            canTakeSpell = true;
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.transform.tag == "Player")
-            canTakeSpell = false;
-    }
 }
