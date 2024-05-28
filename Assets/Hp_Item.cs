79d3d2d0fd10fb82ea443341b55269bacc24dc01
Daniel1nOw <87197476+Daniel1nOw@users.noreply.github.com>
2024-05-28 12:39:56 +0200

    zero zmian

--- Assets/Hp_Item.cs
+++ Assets/Hp_Item.cs
@@ -5,30 +5,8 @@ using UnityEngine;
 
 public class Hp_Item : ItemOnShop
 {
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (!collision.CompareTag("Player"))
-            return;
-
-
-        playerIsClose = true;
-
-        name.text = m_ScriptableObject.Name;
-        name.text += "  Cost: " + m_ScriptableObject.Cost.ToString();
-        description.text = m_ScriptableObject.Description;
-        spriteRenderer.sprite = m_ScriptableObject.ImageItem;
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-            playerIsClose = false;
-    }
-
     public override void UseFunction(float x)
     {
-        print(player.name);
         player.GetComponent<PlayerStats>().ChangeMaxHp(x);
     }
 }
