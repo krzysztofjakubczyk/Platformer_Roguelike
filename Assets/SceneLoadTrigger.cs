01bd349d8386c52f43d6fa4864132742513e3532
krzysztofjakubczyk <28901@s.pm.szczecin.pl>
2024-05-28 21:13:38 +0200

    działające ładowanie i odładowywanie sceny z trigerami/skryptami/przejsciami

--- Assets/SceneLoadTrigger.cs
+++ Assets/SceneLoadTrigger.cs
@@ -2,37 +2,45 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoadTrigger : MonoBehaviour
-{
+{ //oczywiscie sceny do zaladowania beda wybierane losowo, na czas testow sa wybierane przez nazwe podana w inspektorze
+    [SerializeField]MapTranistion mapInstance;
     [Header("Name of scenes to make visible or not")]
     [SerializeField] string[] _scenesToLoad;
     [SerializeField] string[] _scenesToUnLoad;
-
+    GameObject _OutsideDoors; //drzwi umozliwiajace graczowi przejscie do nastepnego pokoju
+    GameObject _InsideDoors; //drzwi uniemozliwiajace graczowi przejscie do poprzedniego pokoju
     private GameObject _player;
 
     void Start()
     {
         _player = GameObject.FindGameObjectWithTag("Player");
+        _OutsideDoors = GameObject.Find("OutDoors");
+        _InsideDoors = mapInstance.FindDisabledObjectByName<Transform>("InDoors")?.gameObject; //to pewnie nie za dziala jak nie bedzie inside i wywali blad
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
-        {
+        if (collision.CompareTag("Player") && gameObject.name == "LoadRoomTrigger")
+        {//jezeli jest to gracz i jest to tiger ladujacy pokoj
             LoadScene();
+        }
+        else if (collision.CompareTag("Player") && gameObject.name == "UnLoadRoomTrigger")
+        {//jezeli jest to gracz i jest to tiger odladowujacy pokoj
             UnLoadScene();
         }
     }
 
     private void UnLoadScene()
     {
-        for (int i = 0; i < _scenesToUnLoad.Length; i++)
+        _InsideDoors.SetActive(true); //wlacz drzwi zeby nie mozna bylo sie cofnac
+        for (int i = 0; i < _scenesToUnLoad.Length; i++) //wylacz scene poprzednia (narazie statycznie podana)
         {
             for (int j = 0; j < SceneManager.sceneCount; j++)
             {
-                Scene loadScene = SceneManager.GetSceneAt(j);
-                if (loadScene.name == _scenesToUnLoad[i])
+                Scene loadedScene = SceneManager.GetSceneAt(j);
+                if (loadedScene.name == _scenesToUnLoad[i])
                 {
-                    SceneManager.UnloadSceneAsync(_scenesToLoad[i]);
+                    SceneManager.UnloadSceneAsync(_scenesToUnLoad[i]);
                 }
             }
         }
@@ -40,7 +48,8 @@ public class SceneLoadTrigger : MonoBehaviour
 
     private void LoadScene()
     {
-        for (int i = 0; i < _scenesToLoad.Length; i++)
+        _OutsideDoors.SetActive(false); //wylacz drzwi zeby mozna bylo przejsc dalej
+        for (int i = 0; i < _scenesToLoad.Length; i++)//dograj scene nastepna
         {
             bool isSceneLoaded = false;
             for (int j = 0; j < SceneManager.sceneCount; j++)
--- Assets/Scripts/MapsTransition.cs
+++ Assets/Scripts/MapsTransition.cs
@@ -2,26 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MapTranistion : MonoBehaviour
 {
-    [SerializeField] GameObject _doors;
+    SceneLoadTrigger _loadTrigger;
     MoneyManager moneyManager;
     int howMoneyFromEnemy;
-    [SerializeField]int howManyEnemies;
+    [SerializeField] int howManyEnemies;
+
     private void Start()
-    { 
+    {
         Entity.OnEnemyDeath += killAnEnemy;
-        howManyEnemies = (GameObject.FindGameObjectsWithTag("Enemy").Length) / 2;
         moneyManager = FindAnyObjectByType<MoneyManager>();
+        getEnemies();
+        // ZnajdŸ wy³¹czony obiekt LoadRoomTrigger
+        _loadTrigger = FindDisabledObjectByName<SceneLoadTrigger>("LoadRoomTrigger");
     }
+
     void killAnEnemy()
     {
-        howManyEnemies--;  
-        Debug.Log(howManyEnemies);
-        if (howManyEnemies == 0)
+        getEnemies();
+        if (howManyEnemies == 1)
         {
-            _doors.SetActive(false);
             moneyManager.AddMoney(howMoneyFromEnemy);
+            if (_loadTrigger != null)
+            {
+                _loadTrigger.gameObject.SetActive(true);
+            }
         }
     }
+
+    private void getEnemies()
+    {
+        howManyEnemies = (GameObject.FindGameObjectsWithTag("Enemy").Length) / 2;
+    }
+
+    // Metoda do znajdowania wy³¹czonych obiektów w scenie
+    public T FindDisabledObjectByName<T>(string name) where T : Component
+    {
+        foreach (T obj in Resources.FindObjectsOfTypeAll<T>())
+        {
+            if (obj.gameObject.name == name)
+            {
+                return obj;
+            }
+        }
+        return null;
+    }
+
 }
