fc6c4f71c8680abf3d2b1a4c407f0ab6e77070e6
Daniel1nOw <87197476+Daniel1nOw@users.noreply.github.com>
2024-05-29 08:09:24 +0200

    new items update testv1

--- Assets/Hp_Item.cs
+++ Assets/Hp_Item.cs
@@ -1,12 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+
 public class Hp_Item : ItemOnShop
 {
+    public List<StatInfo> statInfo = new();
+    [SerializeField] int testowy = 5;
+
     public override void UseFunction(float x)
     {
         player.GetComponent<PlayerStats>().ChangeMaxHp(x);
+        
     }
 }
+
---
+++ Assets/statinfoclass.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatInfo
+{
+    public Itemeees itemes;
+    public float stat;
+}
+
+public enum Itemeees { item1, item2, item3, item4 }
