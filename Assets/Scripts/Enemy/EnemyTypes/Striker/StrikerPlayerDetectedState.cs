5889e57c2a27a3e889b1f84d7056fdd56a70f39a
dawidskoczyk <dawidskoczyk@gmail.com>
2024-06-11 12:47:55 +0200

    wykrywanie gracza Striker

---
+++ Assets/Scripts/Enemy/EnemyTypes/Striker/StrikerPlayerDetectedState.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrikerPlayerDetectedState : PlayerDetected
+{
+    private Striker enemy;
+    public StrikerPlayerDetectedState(Entity entity, BaseStateMachine stateMachine, string animBoolName, PlayerDetectedData stateData, Striker enemy) : base(entity, stateMachine, animBoolName, stateData)
+    {
+        this.enemy = enemy;
+    }
+
+    public override void DoChecks()
+    {
+        base.DoChecks();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+        Debug.Log(isEnemyInRangeToCharge + " czy przeciwnik jest w zasiêgu ataku");
+        if (isEnemyInRange)
+        {
+            entity.Flip();
+            stateMachine.ChangeState(enemy.moveState);
+        }
+        //else if (performCloseRangeAction) stateMachine.ChangeState(enemy.meleeAttackState);
+        //else if (!isEnemyInRangeToCharge && performLongRangeAction) stateMachine.ChangeState(enemy.chargeState);
+        //else if (!isPlayerInMaxAgroRange) stateMachine.ChangeState(enemy.lookForPlayerState);
+        else if (!isDetectingLedge)
+        {
+            entity.Flip();
+            stateMachine.ChangeState(enemy.moveState);
+        }
+
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+}
