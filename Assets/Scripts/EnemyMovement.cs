adebf8f8a7693bcaa9b7318b7367712c2a626d57
dawidskoczyk <dawidskoczyk@gmail.com>
2024-06-24 09:16:22 +0200

    robienie builda

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,22 +1,22 @@
-using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
+//using UnityEngine;
+//using static UnityEditor.Experimental.GraphView.GraphView;
 
-public class EnemyMovement : EnemyMovementInterface
-{
-    GameObject player;
-    Rigidbody2D _rb;
-    float _movementSpeed = 1f;
+//public class EnemyMovement : EnemyMovementInterface
+//{
+//    GameObject player;
+//    Rigidbody2D _rb;
+//    float _movementSpeed = 1f;
 
-    void Start()
-    {
-        _rb = gameObject.GetComponent<Rigidbody2D>();
-        player = GameObject.FindWithTag("Player");
-    }
+//    void Start()
+//    {
+//        _rb = gameObject.GetComponent<Rigidbody2D>();
+//        player = GameObject.FindWithTag("Player");
+//    }
 
-    private void FixedUpdate()
-    {
-        StartRightToLeftMove(_rb, _movementSpeed);
-        EnemySawPlayer(player, _rb, _movementSpeed);
-    }
-}
+//    private void FixedUpdate()
+//    {
+//        StartRightToLeftMove(_rb, _movementSpeed);
+//        EnemySawPlayer(player, _rb, _movementSpeed);
+//    }
+//}
 
