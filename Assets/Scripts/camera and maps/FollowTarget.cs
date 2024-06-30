37966cdf179ed1d8e94ba0422a6cdc76ddec5e6f
dawidskoczyk <dawidskoczyk@gmail.com>
2024-07-01 00:17:33 +0200

    błąd

--- Assets/Scripts/camera and maps/FollowTarget.cs
+++ Assets/Scripts/camera and maps/FollowTarget.cs
@@ -1,61 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using TMPro;
+//using UnityEngine;
 
-public class FollowTarget : MonoBehaviour
-{
-    private Vector3 offset = new Vector3(1f, 0, -10f);
-    private Vector3 velocity = Vector3.zero;
+//public class FollowTarget : MonoBehaviour
+//{
+//    private Vector3 offset = new Vector3(1f, 0, -10f);
+//    private Vector3 velocity = Vector3.zero;
 
-    [SerializeField] float smoothTime = 0.15f;
-    [SerializeField] Transform target;
+//    [SerializeField] float smoothTime = 0.15f;
+//    [SerializeField] Transform target;
 
-    bool camSide;
-    bool notYetChanged_Y;
-    bool notGrounded;
+//    bool camSide;
+//    bool notYetChanged_Y;
+//    bool notGrounded;
 
-    private void Awake()
-    {
-        Application.targetFrameRate = 120;
-    }
+//    private void Awake()
+//    {
+//        Application.targetFrameRate = 120;
+//    }
 
-    private void Start()
-    {
+//    private void Start()
+//    {
 
-        camSide = true;
-        notYetChanged_Y = true;
-        notGrounded = true;
-    }
+//        camSide = true;
+//        notYetChanged_Y = true;
+//        notGrounded = true;
+//    }
 
-    private void Update()
-    {
-        if(target.GetComponent<PlayerMovement>().IsGrounded() && notGrounded)// && notYetChanged_Y)
-        {
+//    private void Update()
+//    {
+//        if(target.GetComponent<PlayerMovement>().IsGrounded() && notGrounded)// && notYetChanged_Y)
+//        {
 
-            Invoke(nameof(Ychange), 0.1f);
-            notGrounded = false;
-            Invoke(nameof(moznajuzskakacznowu), 0.1f);
+//            Invoke(nameof(Ychange), 0.1f);
+//            notGrounded = false;
+//            Invoke(nameof(moznajuzskakacznowu), 0.1f);
 
-        }
+//        }
 
        
-        Vector3 targetPosition = new Vector3( target.position.x + offset.x,  offset.y, -10);
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
-    }
+//        Vector3 targetPosition = new Vector3( target.position.x + offset.x,  offset.y, -10);
+//        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+//    }
 
-    void timeBack()
-    {
-        smoothTime = 0.15f;
-    }
+//    void timeBack()
+//    {
+//        smoothTime = 0.15f;
+//    }
 
-    void Ychange()
-    {
-        offset.y = target.transform.position.y + 2f;
-    }
+//    void Ychange()
+//    {
+//        offset.y = target.transform.position.y + 2f;
+//    }
 
-    void moznajuzskakacznowu()
-    {
-        notGrounded = true;
-    }
-}
+//    void moznajuzskakacznowu()
+//    {
+//        notGrounded = true;
+//    }
+//}
